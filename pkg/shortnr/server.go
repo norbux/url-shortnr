edf4f985405d22f38024260f30ab44732173dce7
Norberto Martínez <norbux@live.com>
2022-12-23 08:10:51 -0600

    Added server interface

--- pkg/shortnr/server.go
+++ pkg/shortnr/server.go
@@ -1,8 +1,12 @@
 package shortnr
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/norbux/url-shortnr/models"
 )
 
 type Server interface {
@@ -13,19 +17,44 @@ type Server interface {
 }
 
 type service struct {
-	Client *mongo.Client
+	Client   *mongo.Client
+	Database string
 }
 
-func NewService(client *mongo.Client) Server {
-	return &service{Client: client}
+func NewService(client *mongo.Client, db string) Server {
+	return &service{Client: client, Database: db}
 }
 
 func (s *service) NewXxh3(c *fiber.Ctx) error {
-	return nil
+	nextSeq, err := NextSeq(s.Client, s.Database)
+	if err != nil {
+		return err
+	}
+
+	hash, err := Xxh3Hash(strconv.Itoa(nextSeq))
+	if err != nil {
+		return err
+	}
+
+	resp := models.HashResponse{Hash: hash}
+
+	return c.JSON(resp)
 }
 
 func (s *service) NewB62(c *fiber.Ctx) error {
-	return nil
+	nextSeq, err := NextSeq(s.Client, s.Database)
+	if err != nil {
+		return err
+	}
+
+	hash, err := B62Hash(nextSeq)
+	if err != nil {
+		return err
+	}
+
+	resp := models.HashResponse{Hash: hash}
+
+	return c.JSON(resp)
 }
 
 func (s *service) GetURLXxh3(c *fiber.Ctx) error {
